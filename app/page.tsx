62d7bc753daee54fb2097d918af51f13852667c1
Simon <24327053+simonslist@users.noreply.github.com>
2024-01-25 08:00:44 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,39 +2,46 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between py-5 px-24">
+    <main className="flex min-h-screen flex-col items-center justify-between py-10 lg:px-24">
 
-<nav className="flex fixed items-center w-screen">
+<nav className="flex items-center w-screen">
 
     <div className="flex-1 flex justify-center mr-auto">
-    <p className="justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          instagram.com/slist.app
-        </p>
+<a href="#">@slist.app
+</a>          
+       
     </div>
 
     <div className="mx-12"><Image
           className="relative"
           src="/logo-icon.svg"
           alt="Next.js Logo"
-          width={50}
+          width={45}
           height={37}
           priority
         /></div>
 
     <div className="flex-1 flex justify-center ml-auto">
-    <p className="justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          merch
-        </p>
+   <a href="#">merch</a>
 
     </div>
 </nav>
 
     
 
-      <div className="iframe-wrapper-1 pt-60">
+      <div className="iframe-wrapper-1 pt-36">
   <div className="iframe-wrapper-2">
-    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfWzZDUvTjvVNJyBJopRyVrnFViPliWziG_zFz-YHNU-FooYw/viewform?embedded=true" width="640" height="493">Loading...</iframe>
+  <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfWzZDUvTjvVNJyBJopRyVrnFViPliWziG_zFz-YHNU-FooYw/viewform?embedded=true" width="640" height="2100">Loading…</iframe>
   </div>
+</div>
+
+<div className="py-16">
+<iframe
+      title="SLIST_CDMX checkout widget"
+      src="https://nas.io/checkout-widget?communityCode=SLIST_CDMX&communitySlug=%2Fslist&buttonText=Join%20for%20early%20access%20to%20lists&buttonTextColorHex=%23fff&buttonBgColorHex=%23ff0000&widgetTheme=dark&backgroundColorHex=%23000"
+      width="100%"
+      height="320"
+    ></iframe>
 </div>
 
     </main>
