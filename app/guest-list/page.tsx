eb0c8ed81d11e03ace7e7a18e69b96e1569a8652
Simon <24327053+simonslist@users.noreply.github.com>
2024-01-26 18:26:46 -0500

    size

--- app/guest-list/page.tsx
+++ app/guest-list/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
             className="w-full"
             src="https://docs.google.com/forms/d/e/1FAIpQLSfWzZDUvTjvVNJyBJopRyVrnFViPliWziG_zFz-YHNU-FooYw/viewform?embedded=true"
             width="100%"
-            height="2151"
+            height="2851"
           >
             Loading…
           </iframe>
