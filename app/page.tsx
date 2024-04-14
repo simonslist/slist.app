5672f29ee1dd50932b646a9267fde72dd225b6d2
Simon <24327053+simonslist@users.noreply.github.com>
2024-04-13 21:05:01 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,31 +3,6 @@ import Image from "next/image";
 // pages/contact.jsx
 import Script from "next/script";
 import { useState, useEffect } from "react";
-import { Metadata } from "next"; // if using TypeScript
-
-export const metadata: Metadata = {
-  openGraph: {
-    title: "Next.js",
-    description: "The React Framework for the Web",
-    url: "https://nextjs.org",
-    siteName: "Next.js",
-    images: [
-      {
-        url: "https://nextjs.org/og.png",
-        width: 800,
-        height: 600,
-      },
-      {
-        url: "https://nextjs.org/og-alt.png",
-        width: 1800,
-        height: 1600,
-        alt: "My custom alt",
-      },
-    ],
-    locale: "en_US",
-    type: "website",
-  },
-};
 
 export default function Home() {
   const [loaded, setLoaded] = useState(false);
